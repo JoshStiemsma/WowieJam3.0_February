f1d96e3f593fabbb95dfa7a2b32ec26ce6627dc3
joshstiemsma <joshstiemsma@live.com>
2021-02-26 22:09:16 -0500

    main re-init

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+
+    public enum Scene{
+        Betting,
+        Fighting
+    }
+
+    public Scene CurrentScene
+    {
+        set { m_currentScene = value; }
+        get { return m_currentScene; }
+    }
+
+    private Scene m_currentScene;
+
+    void Start()
+    {
+        CurrentScene = Scene.Betting;
+    }
+
+
+
+
+    public void SetSceneFighting()
+    {
+        SetSceneType(Scene.Fighting);
+    }
+
+    private void SetSceneType(Scene s)
+    {
+        CurrentScene = s;
+    }
+
+
+}
