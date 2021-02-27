caeeae47de8d6ae8ff57165f6a5da03d10e4727b
Unknown <drummond.jacob13@gmail.com>
2021-02-26 23:44:26 -0500

    movement and level

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Rigidbody2D rb;
+    public float thrust = 5;
+    public float maxSpeed = 5f;
+    bool grounded;
+
     public enum Player
     {
         Left,
@@ -12,12 +18,44 @@ public class PlayerController : MonoBehaviour
 
     public Player playerType;
 
-    public void Update()
+    void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    
+    public void Update()
+    {
+        if(Input.GetKey("d"))
+        {
+           rb.AddForce(transform.right * thrust);
+        }
+         if(Input.GetKey("a"))
+        {
+           rb.AddForce(-transform.right * thrust);
+        }
+         if(Input.GetKeyDown("w") && grounded == true)
+        {
+           rb.AddForce(transform.up * (thrust * 100));
+           grounded = false;
+        }
+
+         if(rb.velocity.magnitude > maxSpeed)
+         {
+                rb.velocity = rb.velocity.normalized * maxSpeed;
+         }
+
+        
+
+
+    }
+         void OnCollisionEnter2D(Collision2D col)
+    {
+       
+       if (col.gameObject.tag == "Ground")
+       {
+           grounded = true;
+        }
+    }
 
 
 }
